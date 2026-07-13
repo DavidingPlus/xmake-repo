403ccb03e03ba7feeec0c2cd154ce33066b79d92
DavidingPlus <davidingplus@qq.com>
2026-07-13 15:43:25 +0800

    update xmake repo

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,6 @@
 add_rules("mode.debug", "mode.release")
 
-add_repositories("davidingplus https://gitcode.com/liuzhixue/xmake-repo.git")
+add_repositories("davidingplus https://github.com/DavidingPlus/xmake-repo.git")
 
 
 includes("src")
