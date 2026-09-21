6c6e821744e35e00fb7c219e0e52dffe5dfe33a8
DavidingPlus <davidingplus@qq.com>
2026-09-21 14:43:51 +0800

    Allow package test to use either library type

--- src/x/xmake-project/main.cpp
+++ src/x/xmake-project/main.cpp
@@ -3,17 +3,6 @@
 #include <xmake-project/ltest.h>
 
 
-#if defined(D_OS_WIN32)
-#ifndef D_BUILD_SHARED
-#error "The consumer must define D_BUILD_SHARED when using the Windows DLL"
-#endif
-
-#ifdef D_DLL_EXPORT
-#error "The consumer must not define D_DLL_EXPORT"
-#endif
-#endif
-
-
 int main()
 {
     std::cout << LTest::foo() << std::endl;
